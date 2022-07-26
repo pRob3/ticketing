d4dfa6ddc8c9a7fd76a04b7a3984b7975497787a
pRob3 <bubblan@slaf.se>
2022-07-26 21:02:08 +0200

    Added validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
 const router = express.Router();
 
-router.post('api/users/signup', (req, res) => {
-  res.send('Hello world!');
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    // Create a new user
+    console.log('Creating a new user');
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
