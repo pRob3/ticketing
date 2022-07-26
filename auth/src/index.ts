e8e7efb3c46f82b864fb8f1b4de783873f89e123
pRob3 <bubblan@slaf.se>
2022-07-26 18:30:44 +0200

    Ingress-Nginx Setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hello WORLD!');
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
