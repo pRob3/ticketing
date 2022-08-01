2656473d8de069891b903f7bee72c7ba3f1f94a1
pRob3 <bubblan@slaf.se>
2022-08-01 12:24:51 +0200

    sign out

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-router.post('/api/users/signout', (req, res) => {
-  res.send('Hello world!');
+router.get('/api/users/signout', (req, res) => {
+  req.session = null;
+  res.send({ message: 'You have been signed out' });
 });
 
 export { router as signoutRouter };
