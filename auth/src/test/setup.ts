5757a3a8e6f1e311a48bb4cc60c88acffb918702
pRob3 <bubblan@slaf.se>
2022-08-02 16:31:58 +0200

    First Test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,7 @@ import { app } from '../app';
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = 'myRandomTestString';
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
