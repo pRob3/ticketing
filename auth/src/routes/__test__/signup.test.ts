913d500ec1f2427b89074a85092b142f5aaf805d
pRob3 <bubblan@slaf.se>
2022-08-02 17:00:23 +0200

    Testing Invalid Input

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,35 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with a invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with a invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '123',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com' })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({ password: 'password' })
+    .expect(400);
+});
