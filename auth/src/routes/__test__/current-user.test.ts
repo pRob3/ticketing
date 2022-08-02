54442560e4db24069334e4f164f68ed847b1a465
pRob3 <bubblan@slaf.se>
2022-08-02 18:40:32 +0200

    Auth Helper Function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,16 +1,9 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { signupRouter } from '../signup';
 
 it('responds with details about the current user', async () => {
-  const signupResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201);
-
-  const cookie = signupResponse.get('Set-Cookie');
+  const cookie = await global.getAuthCookie();
 
   const response = await request(app)
     .get('/api/users/currentuser')
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,9 +1,13 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
 
-let mongo: any;
+declare global {
+  var getAuthCookie: () => Promise<string[]>;
+}
 
+let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'myRandomTestString';
   mongo = await MongoMemoryServer.create();
@@ -26,3 +30,20 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.getAuthCookie = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
