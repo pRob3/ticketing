904501746efb45d9469c69e362ddece2a3cef197
pRob3 <bubblan@slaf.se>
2022-08-03 11:49:47 +0200

    onSuccess Callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 const Signup = () => {
@@ -12,11 +12,12 @@ const Signup = () => {
       email,
       password,
     },
+    onSuccess: () => Router.push('/'),
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    doRequest();
+    await doRequest();
   };
 
   return (
