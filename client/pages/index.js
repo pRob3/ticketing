78226ce0d16220ad964851af92b2c8ceaab2e371
pRob3 <bubblan@slaf.se>
2022-08-03 14:37:21 +0200

    On the Server or the Browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,35 @@
-const LandingPage = ({ color }) => {
-  console.log('I am in the component', color);
-  return <h1>Landing Page!</h1>;
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
+  console.log(currentUser);
+
+  return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = () => {
-  console.log('I am in getInitialProps on the server');
-  return { color: 'pink' };
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx.ingress-nginx...yadayada
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+  return {};
 };
 
 export default LandingPage;
