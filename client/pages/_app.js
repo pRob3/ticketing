013f750635c77352ea917ec8272eb0ea7c46bb69
pRob3 <bubblan@slaf.se>
2022-08-04 10:39:47 +0200

    Passing Props Through

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import buildClient from '../api/build-client';
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1>Header, {currentUser.email}</h1>
       <Component {...pageProps} />
     </div>
   );
@@ -21,7 +21,10 @@ AppComponent.getInitialProps = async (appContext) => {
 
   console.log(pageProps);
 
-  return data;
+  return {
+    pageProps,
+    ...data,
+  };
 };
 
 export default AppComponent;
