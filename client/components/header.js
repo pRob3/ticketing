3d4800cb0f3af8f733f91c76a69ea7387d94621a
pRob3 <bubblan@slaf.se>
2022-08-05 12:36:10 +0200

    Conditionally Showing Links

--- client/components/header.js
+++ client/components/header.js
@@ -1,16 +1,32 @@
 import Link from 'next/link';
 
 export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'Sign up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign in', href: '/auth/signin' },
+    currentUser && { label: 'Sign out', href: '/api/users/signout' },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className='nav-item'>
+          <Link href={href}>
+            <a className='nav-link'>{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
   return (
     <nav className='navbar navbar-light bg-light'>
-      <Link href='/'>
-        <a className='navbar-brand'>Ticketing</a>
-      </Link>
+      <div className='container-fluid'>
+        <Link href='/'>
+          <a className='navbar-brand'>Ticketing</a>
+        </Link>
 
-      <div className='d-flex justify-content-end'>
-        <ul className='nav d-flex align-items-center'>
-          {currentUser ? 'Sign out' : 'Sign in/up'}
-        </ul>
+        <div className='d-flex'>
+          <ul className='nav'>{links}</ul>
+        </div>
       </div>
     </nav>
   );
