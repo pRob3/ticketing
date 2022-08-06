777f46aac96158d184566959aa121a7a7ae174e4
pRob3 <bubblan@slaf.se>
2022-08-06 06:10:41 +0200

    Validating Title and Price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@slafhas/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@slafhas/common';
 const router = express.Router();
 
 router.post(
   '/api/tickets',
   requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     res.sendStatus(200);
   }
