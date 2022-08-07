adc34aeceb6bcd2c376c4968b4416fc1e728068c
pRob3 <bubblan@slaf.se>
2022-08-07 21:03:59 +0200

    Refactoring

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,16 @@
+import { Message } from 'node-nats-streaming';
+import { Listener } from './base-listener';
+
+export class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data:', data);
+
+    setTimeout(() => {
+      console.log(`Finished processing ticket: ${data.id}`);
+      msg.ack();
+    }, 2000);
+  }
+}
