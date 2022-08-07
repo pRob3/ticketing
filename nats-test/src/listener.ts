f51a8c68f7313499f29da010a5f7826e62b9d7d3
pRob3 <bubblan@slaf.se>
2022-08-07 11:30:51 +0200

    Queue Groups

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -12,7 +12,10 @@ const stan = nats.connect('ticketing', clientId, {
 stan.on('connect', () => {
   console.log(`Listener #${clientId} connected to NATS`);
 
-  const subscription = stan.subscribe('ticket:created');
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group'
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
