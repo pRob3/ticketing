11317c720489a2ca88a79f0747605d41f8e544aa
pRob3 <bubblan@slaf.se>
2022-08-07 11:23:30 +0200

    Client ID Generation

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,13 +1,16 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+const clientId = randomBytes(4).toString('hex');
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', clientId, {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
-  console.log('Listener connected to NATS');
+  console.log(`Listener #${clientId} connected to NATS`);
 
   const subscription = stan.subscribe('ticket:created');
 
