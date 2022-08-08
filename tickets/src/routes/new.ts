ba60c0bb002f3c424866b40f6ffafd06b0a656f7
pRob3 <bubblan@slaf.se>
2022-08-08 14:05:28 +0200

    NATS wrapper

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -27,12 +27,12 @@ router.post(
 
     await ticket.save();
 
-    await new TicketCreatedPublisher(client).publish({
-      id: ticket.id,
-      title: ticket.title,
-      price: ticket.price,
-      userId: ticket.userId,
-    });
+    // await new TicketCreatedPublisher(client).publish({
+    //   id: ticket.id,
+    //   title: ticket.title,
+    //   price: ticket.price,
+    //   userId: ticket.userId,
+    // });
 
     res.status(201).send(ticket);
   }
