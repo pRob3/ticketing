cf2d5f964733ba951b2a42d5f4f8f054ef711278
pRob3 <bubblan@slaf.se>
2022-08-08 06:04:10 +0200

    Publishing

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@slafhas/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 
 const router = express.Router();
 
@@ -23,7 +24,16 @@ router.post(
       price,
       userId: req.currentUser!.id,
     });
+
     await ticket.save();
+
+    await new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
+
     res.status(201).send(ticket);
   }
 );
