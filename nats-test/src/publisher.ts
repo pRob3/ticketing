910c7b816df2139dc17909a8eedf42779f73df75
pRob3 <bubblan@slaf.se>
2022-08-08 05:19:42 +0200

    Awaiting Event Publication

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,15 +7,19 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
-    id: '123',
-    title: 'slaf concert',
-    price: 69,
-  });
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'slaf concert',
+      price: 69,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
   // const data = JSON.stringify({
   //   id: '123',
