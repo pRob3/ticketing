033a12a4b806e52d20d21fe598b8ce8122ebcc61
pRob3 <bubblan@slaf.se>
2022-08-09 19:25:37 +0200

    Ensuring Mock Invocations

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -77,4 +78,19 @@ it('creates a ticket with valid', async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].title).toEqual(title);
   expect(tickets[0].price).toEqual(69);
+});
+
+it('publishes an event', async () => {
+  const title = 'slafhas';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.getAuthCookie())
+    .send({
+      title,
+      price: 20,
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
