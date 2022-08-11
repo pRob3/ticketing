f8bee527a08388e1a2e7c9fc05bfdbbda0f0ad5c
pRob3 <bubblan@slaf.se>
2022-08-11 08:14:45 +0200

    Users orders test

---
+++ orders/src/__test__/index.test.ts
@@ -0,0 +1,55 @@
+import request from 'supertest';
+import { app } from '../app';
+import { Order, OrderStatus } from '../models/order';
+import { Ticket } from '../models/ticket';
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: 'slaf concert',
+    price: 999,
+  });
+  await ticket.save();
+  return ticket;
+};
+
+it('fetches orders for a specific user', async () => {
+  // Create three tickets
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const userOne = global.getAuthCookie();
+  const userTwo = global.getAuthCookie();
+
+  // Create one order as user #1
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  // Create two orders as user #2
+  const { body: orderOne } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+  const { body: orderTwo } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  // Make a request to fetch orders for user #2
+  const response = await request(app)
+    .get('/api/orders')
+    .set('Cookie', userTwo)
+    .expect(200);
+
+  // Make sure we only get the orders for user #2
+  expect(response.body.length).toEqual(2);
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
+  expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
+  expect(response.body[1].ticket.id).toEqual(ticketThree.id);
+});
