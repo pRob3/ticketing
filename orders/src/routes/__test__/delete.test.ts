7043dd71a6c83622b976f5a594e29ca38f63e6a1
pRob3 <bubblan@slaf.se>
2022-08-14 19:34:53 +0200

    delete test

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+
+// TODO: Add Auth tests
+it.todo('auth and owner tests');
+
+it('Marks an order as cancelled', async () => {
+  // Create a ticket with Ticket Model
+  const ticket = Ticket.build({
+    title: 'Slaf concert',
+    price: 69,
+  });
+  await ticket.save();
+
+  const user = global.getAuthCookie();
+  // Make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  // publishing and event saying this was cancelled
+
+  // expection to make sure the thing is cancelled
+  const updatedOrder = await Order.findById(order.id);
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('emits an order cancelled event');
