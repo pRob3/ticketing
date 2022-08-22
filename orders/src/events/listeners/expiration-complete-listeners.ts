2982552f8a8e00b5ec49b9026fe6c890f2ce5009
pRob3 <bubblan@slaf.se>
2022-08-22 17:47:03 +0200

    Handling and Emitting an expiration event

---
+++ orders/src/events/listeners/expiration-complete-listeners.ts
@@ -0,0 +1,40 @@
+import {
+  Listener,
+  Subjects,
+  ExpirationCompleteEvent,
+  OrderStatus,
+} from '@slafhas/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    console.log('Expiring order: ', data.orderId);
+    const order = await Order.findById(data.orderId).populate('ticket');
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+
+    console.log('Order cancelled!');
+
+    // Publish an event saying that the order has been cancelled
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    // ack the message
+    msg.ack();
+  }
+}
