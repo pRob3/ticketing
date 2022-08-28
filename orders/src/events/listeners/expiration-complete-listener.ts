f2972eabcd06be774553bb8fb08e18e23507afbe
pRob3 <bubblan@slaf.se>
2022-08-28 12:54:59 +0200

    Don't cancel the order if the order is complete

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -20,6 +20,11 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     if (!order) {
       throw new Error('Order not found');
     }
+    // Don't cancel the order if the order is paid/complete
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
+
     order.set({ status: OrderStatus.Cancelled });
     await order.save();
 
