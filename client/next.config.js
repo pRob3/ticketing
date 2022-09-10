8868a71b048df052610f70f64670891a346b3422
pRob3 <bubblan@slaf.se>
2022-09-10 16:14:36 +0200

    next.js Webpack

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 300,
-      aggregateTimeout: 300,
-      ignored: /node_modules/,
-    };
+  webpack: (config) => {
+    config.watchOptions.poll = 300;
     return config;
   },
 };
